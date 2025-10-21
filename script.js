d9f590223061b14525761f124960e5271c695250
JimmyKIM99 <sagejin@naver.com>
2025-10-21 11:08:27 +0900

    4

--- script.js
+++ script.js
@@ -12,3 +12,57 @@ const handleSearch = () => {
         alert("검색어를 입력하세요");
     }
 };
+
+const viewItem = () => {
+    gtag("event", "view_item", {
+        category: "IT",
+        labels: "promotion_item",
+        currency:"KRW",
+        value: 100000,
+        items: [{
+            item_id: "AZ102",
+            item_name: "소니-amoox6",
+            currency: "KRW",
+            price: 1000000,
+            quantity: 1,
+        }]
+    });
+    alert("view_item 이벤트 발생")
+};
+
+const addToCart = () => {
+    gtag("event", "add_to_cart", {
+        category: "IT",
+        labels: "promotion_item",
+        currency:"KRW",
+        value: 100000,
+        items: [{
+            item_id: "AZ102",
+            item_name: "소니-amoox6",
+            currency: "KRW",
+            price: 1000000,
+            quantity: 1,
+        }]
+    });
+    alert("add_to_cart 이벤트 발생")
+};
+
+const parchase = () => {
+    gtag("event", "purchase", {
+        category: "IT",
+        transaction_id: "A1",
+        aftilation : "온라인몰",
+        currency:"KRW",
+        value: 100000,
+        tax: 10000,
+        shipping : 3000,
+        items: [{
+            item_id: "AZ102",
+            item_name: "소니-amoox6",
+            currency: "KRW",
+            price: 1000000,
+            quantity: 1,
+        }]
+    });
+    alert("purchase 이벤트 발생")
+};
